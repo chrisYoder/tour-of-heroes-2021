02e572498969ce82ff28526dd5c20d9bb8e980e3
chrisYoder <christophermyoder@gmail.com>
2021-07-03 15:04:26 +0000

    First Commit

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { HEROES } from '../mock-heroes';
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+  heroes = HEROES;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+	
+  
+
+}
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+	{id: 11, name: "Kaladin Stormblessed"},
+	{id: 12, name: "Shallan Davar"}, 
+	{id: 13, name: "Dalinar Kholin"},
+	{id: 14, name: "Jasnah Kholin"},
+	{id: 15, name: "Renarin Kholin"}, 
+	{id: 16, name: "Navani Kholin"},
+	{id: 17, name: "Lift"},
+	{id: 18, name: "Rlain"},
+	{id: 19, name: "Venli"}
+]
